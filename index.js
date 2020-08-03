909056f66ffc42cdcc925dbe17890468e4574585
Aiannn <soktoevayan@gmail.com>
2020-08-03 18:38:14 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string) {
+  if (parseInt(string) < 12) {
+    return "Good Morning"
+  }
+  else if (parseInt(string)>12 && parseInt(string)<17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(text) {
+  document.getElementById('greeting').innerText = text 
+}
