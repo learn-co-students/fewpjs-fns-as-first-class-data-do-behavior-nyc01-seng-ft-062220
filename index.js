26e2d0b6819de1fef2b307e2a82ba381af90e59d
axiumone <axiumone@gmail.com>
2020-08-03 13:57:19 -0400

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 
 document.querySelector("button").addEventListener("click", handleClick);
 
+function greet(time) {
+  let hour = time.split(':')[0]
+  if (hour < 12 ) {
+    return ("Good Morning")
+    }
+    else if (hour >= 12 && hour <= 17) {
+      return ("Good Afternoon")
+    } else {
+      return ("Good Evening")
+    }
+}
+
+function displayMessage(greet) {
+  document.querySelector('#greeting').innerText = greet
+}
+
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
