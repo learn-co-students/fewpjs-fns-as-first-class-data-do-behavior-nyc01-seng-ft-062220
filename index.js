8f736b73ce53f8cc58789cc1597644b21b4f168d
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-03 13:23:05 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+
+  let time_split = time.split(':')
+  let hour = time_split[0]
+  hour = parseInt(hour)
+
+  if (hour < 12) {
+   console.log("Good Morning")
+  }else if (hour >= 12 && hour < 17) {
+    console.log("Good afternoon")
+  }else {
+    console.log("Good evening")
+  }
+}
+
 /* Write your implementation of displayMessage() */
+let time_box = document.getElementById('time')
